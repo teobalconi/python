9d495cdca0f94f6197af52c36ea6b26995bef0c8
Teo <teobalconi@gmail.com>
2022-09-13 22:02:09 -0300

    Curso de Python

--- aula123/conta.py
+++ aula123/conta.py
@@ -7,7 +7,8 @@ class Conta:
         self._numero_conta = numero_conta
         self._saldo = saldo
         self._extrato = []
-        if isinstance(saldo, int or float):
+        if isinstance(saldo, (int, float)) and saldo >= 0:
+            self._extrato.append(saldo)
             return
         print(f"Saldo digitado está incorreto: {saldo}")
 
@@ -36,22 +37,23 @@ class Conta:
         self._saldo = saldo
 
     def deposito(self, valor):
-        if (isinstance(valor, float) or isinstance(valor, int)) and valor > 0:
+        if isinstance(valor, (float, int)) and valor > 0:
             self._saldo += valor
             self._extrato.append(valor)
-            print(f"Seu saldo é: R${self._saldo}")
+            print(f"Depósito de R${valor} realizado! Seu saldo é: R${self._saldo}")
         else:
             print(f"Valor digitado ({valor}) não é um número válido")
             return
 
     def extrato(self):
         print("Extrato:", end="\n")
-        for mov in self._extrato:
+        print(f"Saldo anterior: R${self._extrato[0]}", end="\n")
+        for mov in self._extrato[1:]:
             if mov > 0:
                 print(f"Depósito: R${mov}")
-            elif mov < 0:
+            elif mov + 1 < 0:
                 print(f"Saque: R${mov}")
-        print(f"Saldo: R${self._saldo}")
+        print(f"Saldo atual: R${self._saldo}")
 
     @abc.abstractmethod
     def sacar(self, valor):
@@ -62,11 +64,6 @@ class ContaCorrente(Conta):
     def __init__(self, agencia, numero_conta, saldo, limite):
         super().__init__(agencia, numero_conta, saldo)
         self._limite = limite
-        """self.__extrato = []
-        if isinstance(saldo, int) or isinstance(saldo, float):
-            self.__extrato.append(saldo)
-        else:
-            print(f"Saldo digitado está incorreto: {saldo}")"""
 
     @property
     def limite(self):
@@ -77,11 +74,11 @@ class ContaCorrente(Conta):
         self._limite = limite
 
     def sacar(self, valor):
-        if (isinstance(valor, float) or isinstance(valor, int)) and valor > 0:
+        if isinstance(valor, (float, int)) and valor > 0:
             if self._saldo + self._limite >= valor:
                 self._saldo -= valor
                 self._extrato.append(-valor)
-                print(f"Seu saldo é: R${self._saldo}")
+                print(f"Saque de R${valor} realizado. Seu saldo é: R${self._saldo}")
             else:
                 print(f"Saldo insuficiente: R${self._saldo}. Limite: R${self._limite}")
         else:
@@ -89,13 +86,40 @@ class ContaCorrente(Conta):
             return
 
 
+class ContaPoupanca(Conta):
+    def __init__(self, agencia, numero_conta, saldo):
+        super().__init__(agencia, numero_conta, saldo)
+
+    def sacar(self, valor):
+        if isinstance(valor, (float, int)) and valor > 0:
+            if self._saldo >= valor:
+                self._saldo -= valor
+                self._extrato.append(-valor)
+                print(f"Saque de R${valor} realizado. Seu saldo é: R${self._saldo}")
+            else:
+                print(f"Saldo insuficiente: R${self._saldo}.")
+        else:
+            print(f"Valor digitado ({valor}) não é um número válido")
+            return
+
+
 if __name__ == "__main__":
+    ## TESTE CONTA ##
     # c = Conta(1, 123, 0)
     # c.deposito(100)
 
-    cc = ContaCorrente(1, 123, 200, 500)
-    cc.deposito(100)
-    cc.sacar(100)
-    cc.sacar(200)
-    cc.sacar(500)
-    cc.extrato()
+    ## TESTE CONTA CORRENTE ##
+
+    # cc = ContaCorrente(1, 123, 200, 500)
+    # cc.deposito(100)
+    # cc.sacar(100)
+    # cc.sacar(200)
+    # cc.sacar(500)
+    # cc.extrato()
+
+    ## TESTE CONTA POUPANÇA ##
+
+    cp = ContaPoupanca(1, 123, 500)
+    cp.deposito(500)
+    cp.sacar(800)
+    cp.extrato()
